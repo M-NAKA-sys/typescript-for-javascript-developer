cd34a51c83ffc60d5fdaac5bf66c1681d8d28fcc
masa <nakagawa0327@gmail.com>
2025-01-24 22:51:35 +0900

    install ts-node

--- test.js
+++
@@ -1,4 +0,0 @@
-console.log('Start of script'); 
-// ここに他のスクリプトコードを追加 
-console.log('End of script');
-
--- test2.js
+++
@@ -1,9 +0,0 @@
-console.log('Before timeout');
-setTimeout(() => {
-    console.log('Inside timeout');
-}, 1000);
-console.log('After timeout');
-
-
-
-
